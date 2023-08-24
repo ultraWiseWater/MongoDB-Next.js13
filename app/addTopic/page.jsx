e7f6476de9785ab2c791710d5af60bde8a008874
unknown <agigompuu@gmail.com>
2023-08-24 22:18:07 +0900

    frontend finish

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,22 @@
+export default function AddTopic() {
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      ></input>
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      ></input>
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Add Topic
+      </button>
+    </form>
+  );
+}
